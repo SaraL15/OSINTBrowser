384a1659f60cfdcb6bc75f730419bde7824c21d9
Sara Luscombe <sara.luscombe@tutanota.com>
2022-01-18 16:33:34 +0000

    Connection to database complete. INSERT INTO also appears to be working. Filepath column in databse is encrypted.

--- OSINTBrowser/Capture.cs
+++ OSINTBrowser/Capture.cs
@@ -1,5 +1,8 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace OSINTBrowser
@@ -28,8 +31,10 @@ namespace OSINTBrowser
             CaptureType = capturetype; 
         }
 
+       
         public void screenShot()
         {
+            string captureType = "Screenshot";
             //Create a new bitmap.
             var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
 
@@ -39,24 +44,47 @@ namespace OSINTBrowser
             // Take the screenshot from the upper left corner to the right bottom corner.
             gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size,
                                         CopyPixelOperation.SourceCopy);
-
-            // Save the screenshot to the specified path that the user has chosen.
-            //bmpScreenshot.Save("Screenshot.png", ImageFormat.Png);
-            SaveCapture(bmpScreenshot);
+            SaveCapture(bmpScreenshot, captureType);
+            
         }
 
         //Save location of the capture.
-        private void SaveCapture(Bitmap bmp)
+        //**TODO** file name will be casename_date_capturetypeX
+        private void SaveCapture(Bitmap bmp, string captureType)
         {
+            DateTime dateTime = DateTime.Now.ToUniversalTime();
+            string captureDate = DateTime.Now.ToString("yyMMddHHmmss");
+            string captureName = "";
+            string captureLocation = "";
+            //string splitpath = new DirectoryInfo(Case.CaseFilePath).Name;
+            //splitpath = splitpath.Substring(12);
+            
+            //string saveCaptureName = captureDate + "_" + splitpath + "_" + captureType;
             SaveFileDialog saveDlog = new SaveFileDialog();
             saveDlog.InitialDirectory = Case.CaseFilePath;
             saveDlog.FileName = "screenshot.png";
-            saveDlog.Title = "Save Screenshot";
+            captureName = saveDlog.FileName;
+            captureLocation = saveDlog.InitialDirectory;
+            saveDlog.Title = "Save Capture";
             saveDlog.Filter = "PNG File | *.png";
             ImageFormat format = ImageFormat.Png;
             if (saveDlog.ShowDialog() == DialogResult.OK)
             {
                 bmp.Save(saveDlog.FileName);
+                //logCapture(saveCaptureName);
+
+            }
+
+            DbConnect dbc = new DbConnect();
+            dbc.open_connection();
+            dbc.save_to_database(dateTime, captureName, captureLocation);
+        }
+
+        private void logCapture(string captureName)
+        {
+            using (StreamWriter sw = new StreamWriter(System.IO.Path.Combine(Case.CaseFilePath, "Log.txt"), true))
+            {
+                sw.WriteLine("Capture taken: " + captureName, "/n");
             }
         }
     }
---
+++ OSINTBrowser/DbConnect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSINTBrowser
+{
+    public class DbConnect
+    {
+        private string connectionString;
+        SqlConnection cnn;
+
+
+        public void open_connection()
+        {
+            connectionString = @"Data Source=LAPTOP-8VDVE6H0;Initial Catalog=OSIBDatabase;Integrated Security=True";
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            Console.WriteLine("Connection Open");
+        }
+
+        public void close_connection()
+        {
+            cnn.Close();
+        }
+
+        public void save_to_database(DateTime captureDate, string captureName, string captureLocation)
+        {
+            SqlCommand cmd;
+            //SqlDataAdapter adapter = new SqlDataAdapter();
+            string investigatorName = "Matt";
+            byte[] filepath = Encoding.ASCII.GetBytes(captureLocation);
+
+            string query = "INSERT INTO Testcase (date_time, investigator_name, screenshot_name, filepath) " +
+                "VALUES (@date_time, @investigator_name, @screenshot_name, @filepath)";
+            using (cmd = new SqlCommand(query, cnn))
+            {
+                cmd.Parameters.AddWithValue("@date_time", captureDate);
+                cmd.Parameters.AddWithValue("@investigator_name", investigatorName);
+                cmd.Parameters.AddWithValue("@screenshot_name", captureName);
+                cmd.Parameters.Add("@filepath", SqlDbType.VarBinary).Value = filepath;
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+            }
+
+        }
+    }
+
+}
